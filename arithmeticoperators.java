d80a91ca42be42e4001b339504fa2a2fedf987db
kalva pushpalatha <kalvapushpaaiml50@gmail.com>
2025-09-21 18:01:02 +0530

    Create arithmeticoperators.java

---
+++ arithmeticoperators.java
@@ -0,0 +1,11 @@
+public class arithmeticoperators {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 10;
+        System.out.println("a+b:"+ (a+b));
+        System.out.println("a-b:"+(a-b));
+        System.out.println("a*b:"+(a*b));
+        System.out.println("a/b:"+(a/b));
+        System.out.println("a%b:"+(a%b));
+    }
+}
