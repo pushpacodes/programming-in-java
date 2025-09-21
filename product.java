d741ee06fd9ec1449b2da9e0d9bd5e77d8799274
kalva pushpalatha <kalvapushpaaiml50@gmail.com>
2025-09-21 12:52:11 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class product{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float a =sc.nextFloat();
+        float b = sc.nextFloat();
+        float product=a*b;
+        System.out.println(product);
+    }
+}
