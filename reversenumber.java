72bae6443a37797263ec47f0c79d6343c10828ad
kalva pushpalatha <kalvapushpaaiml50@gmail.com>
2025-09-22 18:43:17 +0530

    Update reversenumber.java

--- reversenumber.java
+++ reversenumber.java
@@ -1,3 +1,4 @@
+//method 1
 public class reversenumber {
     public static void main(String args[]){
         int n = 12345678;
@@ -6,5 +7,18 @@ public class reversenumber {
             System.out.print(rem);
             n=n/10;
         }
+    }
+}
+// methd 2
+public class reversenumber{
+    public static void main(String args[]){
+        int i = 123456789;
+        int rev =0;
+        while(i>0){
+            int rem = i%10;
+            rev = (rev * 10 )+rem;
+            i/=10;
+        }
+        System.out.println(rev);
     }
 }
