4f97d32b4c5481ad8f8533f24f08339cd8af13b8
kalva pushpalatha <kalvapushpaaiml50@gmail.com>
2026-07-08 14:53:58 +0530

    Add files via upload

---
+++ nqueens.java
@@ -0,0 +1,55 @@
+public class nqueens{
+    public static void nqueen(char board[][],int row){
+        if(row==board.length){
+            printboard(board);
+            return;
+        }
+        for(int j=0;j<board.length;j++){
+            if(issafe(board,row,j)){
+                board[row][j]='Q';
+                nqueen(board,row+1);//backtracking step
+                board[row][j]='.';
+            }
+        }
+    }
+    public static boolean issafe(char board[][],int row,int col){
+        //vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        //left diagnol
+        for(int i=row-1 ,j=col-1;i>=0&&j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        //right diagnol
+        for(int i=row-1,j=col+1;i>=0&&j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void printboard(char board[][]){
+        System.out.println("-------");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int n=4;
+        char board[][]=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        nqueen(board,0);
+    }
+}
